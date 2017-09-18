9bd13dc54d38886b6b021bb3834db3faa3d3eb05
Akash Varshney <akash.varshney@gmail.com>
2017-09-18 14:21:06 +0530

    added intital project strutcture

---
+++ HackerRank-Algorithms/1.Warmup/1.SolveMeFirst.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HackerRank_Algorithms._1.Warmup
+{
+    public class _SloveMeFirst
+    {
+        public static int SloveMeFirst(int a, int b)
+        {
+            return a + b;
+        }
+
+		public static void CallMethod()
+        {
+            int val1 = Convert.ToInt32(Console.ReadLine());
+            int val2 = Convert.ToInt32(Console.ReadLine());
+            int sum = SloveMeFirst(val1, val2);
+            Console.WriteLine(sum);
+        }
+    }
+}
---
+++ HackerRank-Algorithms/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HackerRank_Algorithms._1.Warmup;
+
+namespace HackerRank_Algorithms
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            _SloveMeFirst.CallMethod();
+        }
+    }
+}
