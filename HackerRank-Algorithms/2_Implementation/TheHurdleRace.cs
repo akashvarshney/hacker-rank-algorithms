d5a7fe293ff9de45282b1164fac4bd8d09cd8899
Akash Varshney <akash.varshney@gmail.com>
2017-09-24 16:03:21 +0530

    added a solution

---
+++ HackerRank-Algorithms/2_Implementation/TheHurdleRace.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HackerRank_Algorithms._2_Implementation
+{
+    public class TheHurdleRace
+    {
+        public static void Call()
+        {
+            string[] tokens_n = Console.ReadLine().Split(' ');
+            int n = Convert.ToInt32(tokens_n[0]);
+            int k = Convert.ToInt32(tokens_n[1]);
+            string[] height_temp = Console.ReadLine().Split(' ');
+            int[] height = Array.ConvertAll(height_temp, Int32.Parse);
+            // your code goes here
+            int heigestvalue = 0;
+            for (int i = 0; i < n; i++)
+            {
+                if (height[i] > heigestvalue)
+                {
+                    heigestvalue = height[i];
+                }
+            }
+            Console.WriteLine(heigestvalue > k ? heigestvalue - k : 0);
+            Console.Read();
+        }
+    }
+}
