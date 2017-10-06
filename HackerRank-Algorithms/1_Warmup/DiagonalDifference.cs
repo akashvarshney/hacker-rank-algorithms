d28e82cbe8e22d7eff6ad392eb4935b30444e421
Akash Varshney <akash.varshney@gmail.com>
2017-10-06 12:48:20 +0530

    added some warm up solutions

---
+++ HackerRank-Algorithms/1_Warmup/DiagonalDifference.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+class Solution {
+
+    static void Main(String[] args) {
+          int result = 0;
+            int n = Convert.ToInt32(Console.ReadLine());
+            int[][] a = new int[n][];
+            for (int a_i = 0; a_i < n; a_i++)
+            {
+                string[] a_temp = Console.ReadLine().Split(' ');
+                a[a_i] = Array.ConvertAll(a_temp, Int32.Parse);
+            }
+            int DignalFirst = 0;
+            int DigonalSecond = 0;
+            for (int i = 0; i < n; i++)
+            {
+                DignalFirst = DignalFirst + a[i][i];
+                DigonalSecond = DigonalSecond + a[i][n - i - 1];
+            }
+            result = DignalFirst > DigonalSecond ? DignalFirst - DigonalSecond : DigonalSecond - DignalFirst;
+        Console.WriteLine(result);
+    }
+}
