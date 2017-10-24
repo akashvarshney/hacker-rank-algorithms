d0e0c3e16a4ef67099f4d287153d99a6681b6377
Akash Varshney <akash.varshney@gmail.com>
2017-10-24 19:58:08 +0530

    added solution for Mars Exploration

---
+++ HackerRank-Algorithms/3Strings/MarsExploration.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HackerRank.Algorithms.Strings
+{
+    public class MarsExploration
+    {
+        public static void Call()
+        {
+            string S = Console.ReadLine();
+            char[] temp = { 'S', 'O', 'S' };
+            List<string> splitedStr = new List<string>();
+            for (int i = 0; i < S.Length; i = i + 3)
+            {
+                splitedStr.Add(S.Substring(i, 3));
+            }
+            int count = 0;
+            foreach (var item in splitedStr)
+            {
+                if (item != "SOS")
+                {
+                    for (int j = 0; j < item.Length; j++)
+                    {
+                        if (item[j] != "SOS"[j])
+                        {
+                            count++;
+                        }
+                    }
+                }
+            }
+            Console.WriteLine(count);
+        }
+    }
+}
--- HackerRank-Algorithms/Program.cs
+++ HackerRank-Algorithms/Program.cs
@@ -7,7 +7,7 @@ namespace HackerRank.Algorithms
     {
         static void Main(string[] args)
         {
-            Gemstones.Call();
+            MarsExploration.Call();
         }
     }
 }
