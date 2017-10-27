bf7866f42a9eeea1e3458e3fa341b6c03f2630ea
Akash Varshney <akash.varshney@gmail.com>
2017-10-27 12:59:52 +0530

    added solution for TheLoveLetterMystery

---
+++ HackerRank-Algorithms/3Strings/TheLoveLetterMystery.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HackerRank.Algorithms.Strings
+{
+    public class TheLoveLetterMystery
+    {
+        static int theLoveLetterMystery(string s)
+        {
+            int transaction = 0;
+            // Complete this function
+            for (int i = 0; i < s.Length / 2; i++)
+            {
+                transaction = transaction + Math.Abs(s[i] - s[s.Length - 1 - i]);
+            }
+            return transaction;
+        }
+
+        public static void Call()
+        {
+            int q = Convert.ToInt32(Console.ReadLine());
+            for (int a0 = 0; a0 < q; a0++)
+            {
+                string s = Console.ReadLine();
+                int result = theLoveLetterMystery(s);
+                Console.WriteLine(result);
+            }
+        }
+    }
+}
--- HackerRank-Algorithms/Program.cs
+++ HackerRank-Algorithms/Program.cs
@@ -7,7 +7,7 @@ namespace HackerRank.Algorithms
     {
         static void Main(string[] args)
         {
-            Anagram.Call();
+            TheLoveLetterMystery.Call();
         }
     }
 }
