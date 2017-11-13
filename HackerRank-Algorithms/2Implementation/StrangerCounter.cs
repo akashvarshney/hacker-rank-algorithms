4c5e5709da8736c79e00ac56658db3f934df32b1
Akash Varshney <akash.varshney@gmail.com>
2017-11-13 15:15:18 +0530

    added solution for Strange counter

---
+++ HackerRank-Algorithms/2Implementation/StrangerCounter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HackerRank.Algorithms.Implementation
+{
+    public class StrangerCounter
+    {
+        public static void Call()
+        {
+            double t = Convert.ToDouble(Console.ReadLine());
+            int n = 2;
+            while (3 * (n - 1) < t) n = 2 * n;
+            Console.WriteLine((3 * (n - 1) - t + 1));
+        }
+    }
+}
---
+++ HackerRank-Algorithms/3Strings/SeparateTheNumbers.cs
@@ -0,0 +1,57 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+
+//namespace HackerRank.Algorithms.Strings
+//{
+//    public class SeparateTheNumbers
+//    {
+//        public static void Cal()
+//        {
+//            int q = Convert.ToInt32(Console.ReadLine());
+//            for (int a0 = 0; a0 < q; a0++)
+//            {
+//                string s = Console.ReadLine();
+
+
+//            }
+//        }
+//        public string chcekSequenceString(string nm)
+//        {
+//            var str = num.toString();
+//            var numLen = str.length;
+//            var firstNum = 0;
+//            var incre = 0;
+//            var testStr = string.Empty;
+
+
+//            if (numLen <= 1)
+//            {
+//                return 'NO';
+//            }
+
+//            for (var i = 1; i <= numLen / 2; i++)
+//            {
+//                firstNum = parseInt(str.substr(0, i));
+//                testStr = firstNum.toString();
+//                incre = firstNum;
+
+//                while (testStr.length < numLen && str != testStr)
+//                {
+//                    // this part needs help in javaScript because it only works up to 53bits
+//                    incre++;
+//                    testStr += incre.toString();
+//                }
+
+//                if (testStr == str)
+//                {
+//                    break;
+//                }
+//            }
+
+//            return testStr == str ? 'YES ' + firstNum : 'NO';
+//        }
+//    }
+//}
